77237a54cfa2b791722f907b9c63bee31fb24023
Edgar Alanis <jezel88@gmail.com>
2022-05-05 18:13:20 -0500

    Update v1.0

--- curp/assets/js/mainScript.js
+++ curp/assets/js/mainScript.js
@@ -20,13 +20,16 @@ function primerVocal(w) {
   return vowels.charAt(0);
 }
 
-/*
-? Función que regrese la primer consonante de una palabra dada
+//? Función que regrese la primer consonante de una palabra dada
 
 function primerCons(w) {
-
+  let aux = w.split('');
+  for (let i = 0; i < aux.length; i++) {
+      if (!(aux[i] === 'A' || aux[i] === 'E' || aux[i] === 'I' || aux[i] === 'O' || aux[i] === 'U' || aux[i] === 'a' || aux[i] === 'e' || aux[i] === 'i' || aux[i] === 'o' || aux[i] === 'u')) {
+          return aux[i];
+      }
+  }
 }
-*/
 
 //* elemento 0 = La inicial y la primer vocal del Apellido Paterno
 curp[0] = apellidoP.substring(0, 1) + primerVocal(apellidoP);
@@ -52,14 +55,14 @@ curp[6] = sexo;
 //* elemento 7 = (abreviatura a 2 letras)
 curp[7] = estado;
 
-//! elemento 8 = Primera consonante no inicial del Apellido Paterno (incompleto)
-//curp[8] = primerCons(apellidoP.substring(1,));
+//* elemento 8 = Primera consonante no inicial del Apellido Paterno (incompleto)
+curp[8] = primerCons(apellidoP.substring(1,));
 
-//! elemento 9 = Primera consonante no inicial del Apellido Materno (incompleto)
-//curp[9] = primerCons(apellidoM.substring(1,));
+//* elemento 9 = Primera consonante no inicial del Apellido Materno (incompleto)
+curp[9] = primerCons(apellidoM.substring(1,));
 
-//! elemento 10 = Primera constante no inicial del nombre
-//curp[10] = primerCons(nombre.substring(1,));
+//* elemento 10 = Primera constante no inicial del nombre
+curp[10] = primerCons(nombre.substring(1,));
 
 //* elemento 11 = Homoclave (2 dígitos al azahar)
 curp[11] = Math.random();
