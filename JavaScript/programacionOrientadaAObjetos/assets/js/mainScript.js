46f9256417ce72533d06bdab2c82a4c3c2a723e3
Edgar Alanis <jezel88@gmail.com>
2022-05-11 14:44:45 -0500

    Change resumenDeJavaScript folder name, and Added programacionOrientadaAObjetos

---
+++ JavaScript/programacionOrientadaAObjetos/assets/js/mainScript.js
@@ -0,0 +1,21 @@
+"use strict"
+
+/*
+* Programación orientada a objetos
+
+Paradigma de la programación que nos permite crear soluciones a problemas que se nos presentan, tomando como ejemplo los objetos del mundo real.
+
+Otras formas de programar:
+
+- Programación funcional
+- Programación imperativa
+- Programación declarativa
+
+* Principios SOLID
+
+
+
+
+
+
+*/
